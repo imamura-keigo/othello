e239d7f91860ac749e0dd0570e8fbe46e7d781c0
imamura-keigo <39424381+imamura-keigo@users.noreply.github.com>
2018-09-11 15:01:50 +0900

    CP

--- main2.py
+++ main2.py
@@ -1,175 +1,164 @@
 #/usr/bin/python3
 from tkinter import *
-import tkinter.messagebox as tkmsg
-import rule 
+import rule
 import time
 
 #running = False
 sub_win = None
-res_win = None
-pass_win = None
 COM = -1
 once = 0
+x = 0
+y = 0
 
 def clickHandle(event):
-	global COM #PL has option 0 : Human Player 1 : Computer
-	global othello
-	xMouse = event.x
-	yMouse = event.y
-	x = -1
-	y = -1
-	BOARD_SIZE = 8 + 1
-	column_width = screen.winfo_width() / BOARD_SIZE
-	row_height = screen.winfo_height() / BOARD_SIZE
+        global x,y,COM,othello #PL has option 0 : Human Player 1 : Computer
+        xMouse = event.x
+        yMouse = event.y
+        x = -1
+        y = -1
+        BOARD_SIZE = 8 + 1
+        column_width = screen.winfo_width() / BOARD_SIZE
+        row_height = screen.winfo_height() / BOARD_SIZE
+        global running
+        
+        if running:
+                for c in range(BOARD_SIZE):
+                        for r in range(BOARD_SIZE):
+                                x1 = c * (column_width)
+                                y1 = r * (row_height)
+                                x2 = x1 + (column_width)
+                                y2 = y1 + (row_height)
+                                if y1 <= yMouse <= y2:
+                                        if x1 <= xMouse <= x2:
+                                                x = c - 1
+                                                y = r - 1
+                                                #print(x,y)
+                                                if (COM == 1) or (COM == 2):
+                                                        if COM != othello.PL_turn:
+                                                                othello.put_stone(x,y,othello.PL_turn)
+                                                                operate_othello()
+                                                                #-------棋譜の表示--------------
+                                                                #othello.show_record()
+                                                                #---------------------
+                                                                sub_win_Update()
+                                                                time.sleep(1)
+                                                        
+                                                        if COM == othello.PL_turn:
+                                                                othello.com_search(othello.PL_turn)
+                                                                x = othello.x_before
+                                                                y = othello.y_before
+                                                                operate_othello()
+                                                        #--------棋譜の表示-------------
+                                                        #othello.show_record()
+                                                        #----------------------
+                                                        sub_win_Update()
 
-	global running
-	if running:
-		for c in range(BOARD_SIZE):
-			for r in range(BOARD_SIZE):
-				x1 = c * (column_width)
-				y1 = r * (row_height)
-				x2 = x1 + (column_width)
-				y2 = y1 + (row_height)
-				if y1 <= yMouse <= y2:
-					if x1 <= xMouse <= x2:
-						x = c - 1
-						y = r - 1
-						if (COM == 1) or (COM == 2):
-							if COM != othello.PL_turn:
-								othello.put_stone(x,y,othello.PL_turn)
-								operate_othello()
-								sub_win_Update()	
-							while COM == othello.PL_turn:
-								othello.com_search(othello.PL_turn)
-								operate_othello()
-								sub_win_Update()
-							sub_win_Update()	
-				
-						elif COM == 0:
-							if othello.put_stone(x,y,othello.PL_turn):
-								operate_othello()
-								sub_win_Update()
-		if othello.finish:
-    			show_result()
 
-	else:
-		if 300<= yMouse <=350:
-			if 25<=xMouse<=155:
-				COM = 0
-				playGame()
-			elif 180<=xMouse<=310:
-				COM = 2
-				playGame()
-			elif 335<=xMouse<=465:
-				COM = 1
-				playGame()
+                                                elif(COM == 0):
+                                                        if othello.put_stone(x,y,othello.PL_turn):
+                                                                operate_othello()
+                                                                #----------------
+                                                                #othello.show_record()
+                                                                #----------------
+                                                                sub_win_Update()
 
+        else:
+                if 300<= yMouse <=350:
+                        if 25<=xMouse<=155:
+                                COM = 0
+                                playGame()
+                        elif 180<=xMouse<=310:
+                                COM = 2
+                                playGame()
+                        elif 335<=xMouse<=465:
+                                COM = 1
+                                playGame()
 
 
 def operate_othello():
-	BOARD_SIZE = 8 + 1
-	column_width = screen.winfo_width() / BOARD_SIZE
-	row_height = screen.winfo_height() / BOARD_SIZE
-	
-	othello.put_checker(othello.PL_turn)
-	if not len(othello.kihu) == 0:
-		i = len(othello.kihu) - 1
-		while True:
-			if len(othello.kihu[i]) == 4:
-				k = othello.kihu[i]
-				break
-			else:
-				i = i-1
+        global x,y,running,PL_turn
+        BOARD_SIZE = 8 + 1
+        column_width = screen.winfo_width() / BOARD_SIZE
+        row_height = screen.winfo_height() / BOARD_SIZE
 
+        for c in range(BOARD_SIZE):
+                for r in range(BOARD_SIZE):
+                        x1 = c * (column_width)
+                        y1 = r * (row_height)
+                        x2 = x1 + (column_width)
+                        y2 = y1 + (row_height)
+                        if (c == x + 1  and r == y + 1 and running ):
+                                screen.create_rectangle(x1,y1,x2,y2,fill = 'red')
+                        elif (c == 0 or r == 0):
+                                screen.create_rectangle(x1,y1,x2,y2,fill = '#222')
+                        else:
+                                screen.create_rectangle(x1,y1,x2,y2,fill = 'green')
+                                # if othello.can_put[(c-1)+(r-1)*8] > 0:
+                                #         othello.put_checker(3-PL_turn)
+                                #         screen.create_rectangle(x1,y1,x2,y2,fill = 'blue')
 
-	for c in range(BOARD_SIZE):
-		for r in range(BOARD_SIZE):
-			x1 = c * (column_width)
-			y1 = r * (row_height)
-			x2 = x1 + (column_width)
-			y2 = y1 + (row_height)
-			
-			if (c == 0 or r == 0):
-					screen.create_rectangle(x1,y1,x2,y2,fill = '#222')
-			else:
-					screen.create_rectangle(x1,y1,x2,y2,fill = 'green')
-					if othello.can_put[(c-1)+(r-1)*8] > 0:
-						screen.create_rectangle(x1,y1,x2,y2,fill = 'pale green')
-					if not len(othello.kihu) == 0:
-						if (c == k[1] + 1  and r == k[2] + 1 and running ):
-							screen.create_rectangle(x1,y1,x2,y2,fill = 'red')
-			if c != 0 and r !=0:    
-				if othello.board[(c-1)+(r-1)*8]==1:
-					screen.create_oval(x1,y1,x2,y2,fill = 'black')
-				elif othello.board[(c-1)+(r-1)*8]==2:
-					screen.create_oval(x1,y1,x2,y2,fill = 'white')
-
-	screen.pack()
-	screen.update()
+                        if c != 0 and r !=0:
+                                #othello.put_checker(PL_turn)
+                                #if othello.can_put[(c-1)+(r-1)*8] > 0:
+                                #        screen.create_rectangle(x2,y1,x2,y2,fill="blue")      
+                                if othello.board[(c-1)+(r-1)*8]==1:
+                                        screen.create_oval(x1,y1,x2,y2,fill = 'black')
+                                elif othello.board[(c-1)+(r-1)*8]==2:
+                                        screen.create_oval(x1,y1,x2,y2,fill = 'white')
+        screen.pack()
+        screen.update()
 
 def show_othello_board():
-	BOARD_SIZE = 8 + 1
-	column_width = screen.winfo_width() / BOARD_SIZE
-	row_height = screen.winfo_height() / BOARD_SIZE
-	for c in range(BOARD_SIZE):
-		for r in range(BOARD_SIZE):
-			x1 = c * (column_width)
-			y1 = r * (row_height)
-			x2 = x1 + (column_width)
-			y2 = y1 + (row_height)
-			if c == 0 or r == 0:
-				xpos = x1+column_width
-				ypos = y1+row_height + row_height/2
-				screen.create_rectangle(x1,y1,x2,y2,fill = '#222')
-			else:
-				screen.create_rectangle(x1,y1,x2,y2,fill = 'green')
+        global x,y
+        BOARD_SIZE = 8 + 1
+        column_width = screen.winfo_width() / BOARD_SIZE
+        row_height = screen.winfo_height() / BOARD_SIZE
+        for c in range(BOARD_SIZE):
+                for r in range(BOARD_SIZE):
+                        x1 = c * (column_width)
+                        y1 = r * (row_height)
+                        x2 = x1 + (column_width)
+                        y2 = y1 + (row_height)
+                        if (c == 0 or r == 0):
+                                xpos = x1+column_width
+                                ypos = y1+row_height + row_height/2
+                                screen.create_rectangle(x1,y1,x2,y2,fill = '#222')
+                        else:
+                                screen.create_rectangle(x1,y1,x2,y2,fill = 'green')
 	
-	screen.pack()
-	screen.update()
-	kihu_bot.pack()
-	kihu_bot.update()
-	# undo_bot.pack()
-	# undo_bot.update()
-	restart_bot.pack()
-	restart_bot.update()
-	quit_bot.pack()
-	quit_bot.update()
-
-def choose():
-	global listbox
-	global othello
-	global screen
-	global COM
-	try:
-		if listbox.curselection()[0] == 0:
-			othello = rule.Board(COM)
-		elif "pass" !=  othello.kihu[listbox.curselection()[0]][1]:
-			for i in range(len(othello.kihu) - listbox.curselection()[0]):
-				othello.PL_turn = int(othello.kihu.pop()[0])
-			othello.board = othello.kihu[-1][3]	#クソざこarray型になっとる
-	except IndexError:
-		value = None
-	self.finish = False
-	sub_win_Update()
-	screen.delete("stone")
-	show_othello_board()
-	operate_othello()
+        screen.pack()
+        screen.update()
+        undo.pack()
+        undo.update()
 
 
 def sub_win_Update():
-	global sub_win
-	global othello
-	global scrollbar
-	global listbox
+        global sub_win
+        global othello
+        global scrollbar
+        global listbox
+        ishi = str("")
+        listbox.delete (first = 0, last=listbox.size())
 
-	listbox.delete (first = 0, last=listbox.size())
-	for kihu in othello.kihu:
-#		if str(kihu) not in listbox:
-			listbox.insert(END, str(kihu))
-			
-	listbox.pack(side=LEFT, fill=BOTH)
-	scrollbar.config(command=listbox.yview)
-	sub_win.update()
+#------------------棋譜の表示-------------------------------------------------
+        for i,kihu in zip(range(len(othello.kihu)),othello.kihu):
+                if len(kihu)!=0:
+                        #パスだった場合
+                        if  kihu[1] == -1:
+                                zahyou = "Pass"
+
+                        #それ以外
+                        else:
+                                zahyou = "abcdefgh" [kihu[1]]+str(kihu[2]+1)
+                if kihu[0]==1:
+                        ishi = '●'
+                elif kihu[0]==2:
+                        ishi = '○'
+                listbox.insert(END, str(i+1)+" :  "+ ishi + " " + str(zahyou) )
+#----------------------------------------------------------------------------
+        listbox.pack(side=LEFT, fill=BOTH)
+        scrollbar.config(command=listbox.yview)
+        sub_win.update()
 
 def create_new_screen():
 	global sub_win
@@ -190,34 +179,30 @@ def create_new_screen():
 		x = (ws/2) - (w/2)
 		y = (hs/2) - (h/2)
 		sub_win.geometry("%dx%d+%d+%d" % (w, h, x, y))
-		msg = Message(sub_win, text="棋譜出してくれ~") #ここに棋譜を貼る
-		msg.pack()
+		# msg = Message(sub_win, text="棋譜出してくれ~") #ここに棋譜を貼る
+		# msg.pack()
 		scrollbar = Scrollbar(sub_win)
 		scrollbar.pack(side=RIGHT, fill=Y)
 		
-		buttonFrame = Frame(sub_win)
-		buttonFrame.pack(side=BOTTOM)
-
-		chooseButton = Button(buttonFrame, text="Choose", command=choose)
-		chooseButton.pack()
-
-		listbox = Listbox(sub_win, yscrollcommand=scrollbar.set, selectmode  = SINGLE)
+		listbox = Listbox(sub_win, yscrollcommand=scrollbar.set)
 		
 		sub_win_Update()
-		
+
 def keyHandle(event):
 	global ruuning
 	symbol = event.keysym
-	if symbol.lower()=="r"  and running: #rでリスタート
+	if symbol.lower()=="r": #rでリスタート
 		runGame()
-	elif symbol.lower()=="q"  and running: #qでおわおわり
+	elif symbol.lower()=="q": #qでおわおわり
 		root.destroy()
 	elif symbol.lower()=="w" and running:
+                #--------csvから棋譜を読み込む-----------
 		create_new_screen()
 	elif symbol.lower()=="u" and running:
-    		print("undo")
-    		othello.Undo()
-	
+                print("undo")
+                othello.Undo(COM)
+                sub_win_Update()
+
 def runGame():
 	global running
 	running = False
@@ -232,53 +217,33 @@ def runGame():
 
 	screen.update()
 
-
-def Pass(PL):
-	''' Passメッセージ呼び出し
-	rule.py Pass()内から呼び出し '''
-	tkmsg.showinfo('パス',PL + 'パスです')
-
-def show_result():
-	global res_win,othello
-	''' 
-	結果表示画面
-	'''
-	if res_win is None or not res_win.winfo_exists():
-		res_win = Toplevel()
-		res_win.title("対戦結果")
-		res_win.geometry("400x300")
-
-		win_player = Label(res_win, text=othello.show_win, font=('Helvetica', '24', 'bold'))
-		win_player.pack(pady = 20,anchor = N)
-
-		display_score = Label(res_win, text=othello.res_score, font=('Helvetica', '18', 'bold'))
-		display_score.pack(pady = 20)
-
-
 def playGame():
-	global board, running
-	global othello
-	global PL2
-	global COM
-	running = True
-	screen.delete(ALL)
+        global board, running,x,y
+        global othello
+        global PL2
+        global COM
 
-	othello = rule.Board(COM)
-
-	create_new_screen() #棋譜用サブ画面表示 
+        screen.delete(ALL)
+        othello = rule.Board(COM)
+        create_new_screen() #棋譜用サブ画面表示 
 	
-	
-	show_othello_board() #ゲームボードの表示
-	
+        
+        show_othello_board() #ゲームボードの表示
 	#オセロの駒表示
+        operate_othello()
+        running = True
+        if COM == 1: #COMが先手の場合。クリック前に先に打つ
+                time.sleep(1)
+                othello.com_search(1)
+                x = othello.x_before
+                y = othello.y_before
+                operate_othello()
+                #-----------------------
+                #othello.show_record()
+                #-----------------------
+                sub_win_Update()
+	#ゲーム結果
 
-	operate_othello()
-	running = True
-	if COM == 1: #COMが先手の場合。クリック前に先に打つ
-		time.sleep(1)
-		othello.com_search(1)
-		sub_win_Update()
-		operate_othello()
 
 if __name__ == '__main__':
 #親画面の初期化処理
@@ -286,17 +251,13 @@ if __name__ == '__main__':
 	root.resizable(0,0)
 	Fboard = Frame(root)
 	bottom = Frame(Fboard)
-	kihu_bot = Button(bottom,text ="棋譜(w)")
-	undo_bot = Button(bottom,text ="Undo(u)")
-	restart_bot = Button(bottom,text ="Restart(r)")
-	quit_bot = Button(bottom,text ="Quit(q)")
-	
+	undo = Button(Fboard,text ="Undo")
 	screen = Canvas(Fboard, width=500, height=500, background="#222",highlightthickness=0)
-#	score = Canvas(Fboard, width=500, height=500, background="#222",highlightthickness=0)
+	#score = Canvas(Fboard, width=500, height=500, background="#222",highlightthickness=0)
+#	screen.create_rectangle(0, 0, 500, 500, fill = 'green')
 	Fboard.pack()
 	screen.pack(anchor = W)
-#	score.pack(anchor = E)
-	bottom.pack()
+	#score.pack(anchor = E)
 	root.wm_title("Othello")
 #ゲームの起動
 	runGame()
@@ -308,15 +269,10 @@ if __name__ == '__main__':
 	root.mainloop()
 """ 
 root ---*--- Fboard ---*--- screen
-        |				|
-		|               *--- bottom
-        |               |
-        |               *---score
+        |              |
+		|              *--- bottom
+		|              |
+		|              *--- score
         |
 		*--- sub_win
-        |
-        |
-        *---res_win
 """
-
-
